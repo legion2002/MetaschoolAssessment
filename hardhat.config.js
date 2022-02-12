4682b3b716d177d63dcb4d5f3426ed91e8da7d75
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-02-12 16:10:58 +0530

    Basic setup done

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL_MUMBAI, PRIVATE_KEY } = process.env;
+module.exports = {
+   solidity: "0.8.0",
+   defaultNetwork: "mumbai",
+   networks: {
+      hardhat: {},
+      mumbai: {
+         url: API_URL_MUMBAI,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
